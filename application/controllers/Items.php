e71be4b86cc0ccadeef58edd378d8f3549c38e85
Luda <luisdavidgd@gmail.com>
2019-05-16 13:41:40 -0500

    Some changes

--- application/controllers/Items.php
+++ application/controllers/Items.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Items extends MY_Controller
 {
 	public function index(){
+        $data = $this->ion_auth_data;
         $this->load->model('Item_model', 'item');
         $data['items'] = $this->item->where('active', 1)->as_array()->get_all();
         $this->load->view('items/items_list', $data);
