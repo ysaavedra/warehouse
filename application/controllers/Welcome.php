1ff32ac6fbde6f9b08f3b244be8803cb19d1869b
Luda <luisdavidgd@gmail.com>
2019-05-16 20:50:28 -0500

    Update some views

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data = $this->ion_auth_data;
+		$this->load->view('welcome_message', $data);
 	}
 }
