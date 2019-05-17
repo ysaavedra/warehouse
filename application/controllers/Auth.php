d66fdf55bb7fc3094e6fceb31d7edcf8ad5c7e52
Luda <luisdavidgd@gmail.com>
2019-05-17 00:18:15 -0500

    Add some data

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -55,6 +55,8 @@ class Auth extends CI_Controller
 				$this->data['users'][$k]->groups = $this->ion_auth->get_users_groups($user->id)->result();
 			}
 
+			$this->data['page_title'] = $this->lang->line('index_heading');
+			$this->data['page_slogan'] = $this->lang->line('index_subheading');
 			$this->_render_page('auth' . DIRECTORY_SEPARATOR . 'index', $this->data);
 		}
 	}
